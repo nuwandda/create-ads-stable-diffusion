8cb2ea60b075fb48ac84d5397443e7f77ea6f804
Bugrahan Donmez <donmezbugrahan@gmail.com>
2024-01-05 23:18:15 +0300

    Add Streamlit

---
+++ main.py
@@ -0,0 +1,30 @@
+import streamlit as st
+from PIL import Image
+import streamlit_services
+
+
+def main():
+    st.title("Welcome to AdCreative Demo App")
+
+    # Upload images
+    encoded_base_img = st.file_uploader("Upload Reference Image", type=["jpg", "jpeg", "png"])
+    logo = st.file_uploader("Upload Logo (Transparent)", type=["png"])
+
+    # Text input fields
+    prompt = st.text_input("Prompt", "Enter text here")
+    hex_color = st.text_input("HEX Color", "Enter text here")
+    punchline_text = st.text_input("Punchline Text", "Enter text here")
+    button_text = st.text_input("Button Text", "Enter text here")
+    punchline_color = st.text_input("Punchline Color", "Enter text here")
+    button_color = st.text_input("Button Color", "Enter text here")
+
+    # Generate image
+    if st.button("Generate"):
+        if encoded_base_img is not None and logo is not None:
+            init_image = Image.open(encoded_base_img)
+            logo_image = Image.open(logo)
+            result = streamlit_services.create_ad(init_image, logo_image, hex_color, prompt, punchline_text, punchline_color, button_text, button_color)
+            st.image(result, caption="Generated Ad", use_column_width=True)
+
+if __name__ == "__main__":
+    main()
---
+++ streamlit_services.py
@@ -0,0 +1,93 @@
+import torch 
+from diffusers import StableDiffusionImg2ImgPipeline
+import os
+from dotenv import load_dotenv
+from PIL import Image
+import utils
+
+load_dotenv()
+MODEL_PATH = os.getenv('MODEL_PATH')
+SEED = -1
+NUM_INFERENCE_STEPS = 50
+GUIDANCE_SCALE = 15
+STRENGTH = 0.6
+NEGATIVE_PROMPT = 'noisy, blurry, amateurish, sloppy, unattractive'
+
+
+def create_pipeline(model_path):
+    # Create the pipe 
+    pipe = StableDiffusionImg2ImgPipeline.from_single_file(
+        model_path, 
+        revision="fp16", 
+        torch_dtype=torch.float16
+    )
+    
+    # pipe = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, use_safetensors=True, variant="fp16")
+
+    if torch.backends.mps.is_available():
+        device = "mps"
+    else: 
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    pipe.to(device)
+    
+    return pipe
+
+
+pipe = create_pipeline('weights/realisticVisionV60B1_v20Novae.safetensors')
+
+
+def create_ad(init_image, logo_image, hex_color, prompt, punchline_text, punchline_color, button_text, button_color,) -> Image:
+    generator = torch.Generator().manual_seed(utils.set_seed()) if float(SEED) == -1 else torch.Generator().manual_seed(int(SEED))
+    init_image = init_image.resize((512, 512))
+    requested_color = utils.hex_to_rgb(hex_color)
+    closest_color_name = utils.get_colour_name(requested_color)
+    
+    final_prompt = 'Retail packaging style {} {}. Vibrant, enticing, commercial, product-focused, eye-catching, professional, highly detailed'.format(closest_color_name, prompt)
+
+    base_image: Image = pipe(final_prompt,
+                        image=init_image,
+                       strength=STRENGTH,
+                        negative_prompt=NEGATIVE_PROMPT,
+                        guidance_scale=GUIDANCE_SCALE, 
+                        num_inference_steps=NUM_INFERENCE_STEPS, 
+                        generator = generator, 
+                    ).images[0]
+    
+    target_image_size = (768, 768)
+    target_resize_width_logo = 150
+    target_resize_width_product = 400
+    
+    result = utils.overlay_images(logo_image, (round(target_image_size[0] / 2) - round(target_resize_width_logo / 2), 0),
+                            target_image_size, target_resize_width_logo, True)
+    empty_button_size = (round(target_image_size[0] / 4 * 3), 20)
+    result = utils.add_semi_rounded_rectangle(result, button_size=empty_button_size, 
+                                              button_position=(target_image_size[0] / 8, -5), 
+                                              button_color=utils.hex_to_rgb(button_color))
+    result = utils.add_semi_rounded_rectangle(result, button_size=empty_button_size, 
+                                              button_position=(target_image_size[0] / 8, target_image_size[1] - 5), 
+                                              button_color=utils.hex_to_rgb(button_color))
+    
+    result = utils.overlay_images(base_image, (round(target_image_size[0] / 2) - round(target_resize_width_product / 2), 
+                                             round(target_image_size[1] / 2) - round(target_resize_width_product / 2)),
+                                (result.width, result.height), target_resize_width_product, False, result)
+    
+    # (x, y) position of the top-left corner of the text
+    text_position = (round(target_image_size[0] / 8), target_image_size[1] - 130)
+    # Replace with the path to your TrueType font file if using a custom font
+    font_path = "Trajan-Regular.ttf"
+    max_width = round(target_image_size[0] * 6 / 8)
+    max_height = 300
+    best_font_size, _, _ = utils.find_best_font_size(punchline_text, max_width, max_height, font_path)
+    result = utils.add_text(result, punchline_text, text_position, best_font_size, utils.hex_to_rgb(punchline_color), font_path)
+    
+    max_width = 200
+    max_height = 50
+    best_font_size, _, _ = utils.find_best_font_size(button_text, max_width, max_height, font_path)
+    button_position = (round(target_image_size[0] / 2) - round(target_resize_width_product / 4), 
+                       target_image_size[1] - 70)
+    result = utils.add_button(result, font_path=font_path, button_position=button_position, 
+                              button_color=utils.hex_to_rgb(button_color), text=button_text, font_size=best_font_size)
+    
+    return result
+        
